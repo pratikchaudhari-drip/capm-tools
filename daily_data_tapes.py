2803df20b2c3f1779446a2904b8d69ecbab50891
Pratik Chaudhari <pratik.chaudhari@dripcapital.com>
2021-11-10 23:11:09 +0530

    Calls seller finance and scf tapes separately, stores in tapes/

---
+++ daily_data_tapes.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from db_connector import db
+from tapes import grabber
+from utils import writer
+
+df_sf = grabber.query_tape()
+df_scf = grabber.query_tape(type= 'scf')
+
+writer.write_file(df_sf, 'csv')
+writer.write_file(df_scf, 'csv')
